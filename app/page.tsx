fa59b86a2f0a861414745629ee086ed41f4c2976
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-07-17 18:36:14 +0400

    convex setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import Tasks from "@/components/todomax/tasks";
 import { ModeToggle } from "@/components/ui/modetoggle";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <h1>Todolist</h1>
       <ModeToggle />
+      <Tasks />
     </main>
   );
 }
---
+++ components/todomax/tasks.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { ModeToggle } from "@/components/ui/modetoggle";
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
+import Image from "next/image";
+import { stringify } from "querystring";
+
+export default function Tasks() {
+  const tasks = useQuery(api.tasks.get);
+  return (
+    <div>
+      <p>Tasks</p>
+      {tasks?.map((task, idx) => <p key={idx}>{JSON.stringify(task)}</p>)}
+    </div>
+  );
+}
